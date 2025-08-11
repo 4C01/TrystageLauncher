ceaad71c02362420593b87af9a8a9d56aeeb677a
AstarLC4036 <1975727002@qq.com>
2025-08-12 01:15:45 +0800

    Features & Decorations

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace trystageClient.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap vivid_blurred_colorful_background {
+            get {
+                object obj = ResourceManager.GetObject("vivid-blurred-colorful-background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
